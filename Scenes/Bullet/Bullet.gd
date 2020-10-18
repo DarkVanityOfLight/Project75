7a30e1bc91f6904d28e3029acc4301327b1935cd
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-10-18 13:34:51 +0200

    feat: Simple bullet + bullet movement

---
+++ Scenes/Bullet/Bullet.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+var speed = 150
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	position += transform.x * speed * delta
+	
+func _on_Bullet_body_entered(body):
+	if body.is_in_group("enemy"):
+		body.queue_free()
+	
+	queue_free()
