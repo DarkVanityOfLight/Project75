ac115ed5991ee5e495b5b5e9bcc13c5673b432b5
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-10-18 13:35:30 +0200

    feat: Simple shooting

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,11 +1,17 @@
 extends KinematicBody2D
 
+var Bullet = preload("res://Scenes/Bullet/Bullet.tscn")
+
 var velocity = Vector2()
 var speed = 250
 
 func _ready():
 	pass
 
+func shoot():
+	var b = Bullet.instance()
+	get_parent().add_child(b)
+	b.transform = $Muzzle.global_transform
 
 func get_input():
 	velocity = Vector2()
@@ -17,11 +23,14 @@ func get_input():
 		velocity.x -= 1
 	if Input.is_action_pressed("walk_right"):
 		velocity.x += 1
-		
+			
 	velocity = velocity.normalized() * speed
 		
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
 
 func _physics_process(delta):
 	get_input()
 	move_and_collide(velocity *  delta)
+	look_at(get_local_mouse_position())
 	
