b687cb06e430feca8abd9830cfb144174716b20d
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-10-19 14:59:22 +0200

    refractor Main Menu to Main_Menu
    refractor Main Menu to Main_Menu

--- Scenes/Menues/Main Menue/LoadGameBtn.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-func _ready():
-	print("Load Game")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
--- Scenes/Menues/Main Menue/NewGameBtn.gd
+++
@@ -1,5 +0,0 @@
-extends Button
-
-
-func _ready():
-	print("New Game")
