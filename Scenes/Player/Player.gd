9d1229d5c38b448de869fab85107fc3fa988e471
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-11-01 16:59:55 +0100

    feat: Implement dodging

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,17 +1,24 @@
 extends KinematicBody2D
 
 var Bullet = preload("res://Scenes/Bullet/Bullet.tscn")
-
+var extents
 var velocity = Vector2()
 export (int) var speed = 250
 
 func _ready():
-	pass
+	extents = $CollisionShape2D.shape.get_extents()
 
 func shoot():
 	var b = Bullet.instance()
 	get_parent().add_child(b)
 	b.transform = $Muzzle.global_transform
+
+func dodge():
+	$CollisionShape2D.shape.set_extents(Vector2(extents.x, extents.y/2))
+	
+	
+func un_dodge():
+	$CollisionShape2D.shape.set_extents(extents)
 
 func get_input():
 	velocity = Vector2()
@@ -30,6 +37,11 @@ func get_input():
 		
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
+		
+	if Input.is_action_just_pressed("dodge"):
+		dodge()
+	if Input.is_action_just_released("dodge"):
+		un_dodge()
 
 func _physics_process(delta):
 	get_input()
