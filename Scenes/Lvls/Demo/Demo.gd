8987900c0f0eda004663c97c5037582d8a1c5523
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-03-26 16:51:19 +0100

    feat: Clean

--- Scenes/Lvls/Demo/Demo.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-const player = preload("res://Scenes/Player/Player.tscn")
-
-
-func _ready():
-	var player_instance = player.instance()
-	player_instance.position = $Spawn.position
-	add_child(player_instance)	
